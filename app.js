3262e6e81253fb13ce292bb94d0001c79cf28149
Hasmodin <hasmodin@gmail.com>
2024-01-17 07:07:41 +0300

    update files

--- app.js
+++ app.js
@@ -7,3 +7,20 @@ function hideSideBar() {
     const sidebar = document.querySelector(".sidebar");
     sidebar.style.display = "none";
 }
+
+// collapsible function
+
+let coll = document.querySelector(".collapsible");
+let i;
+for(i=0; i<coll.length; i++) {
+    coll[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        let content = this.nextElementSibling;
+        if(content.style.maxHeight) {
+            content.style.maxHeight = null;
+
+        } else {
+            content.style.maxHeight = content.scrollHeight + "px";
+        }
+    });
+}
