a2e06515ca51a1a7bce98bd8b3780314621a7d08
Hasmodin <hasmodin@gmail.com>
2024-01-21 10:21:48 +0300

    update files

--- app.js
+++ app.js
@@ -14,12 +14,7 @@ function hideSideBar() {
 
 
 
-// collapsible function
-
-// function toggleClass() {
-//     let coll = document.querySelector(".collapsible-box");
-//     coll.classList.toggle("active");
-// }
+// collapsible functions
 
 let coll = document.querySelectorAll(".collapsible-box");
 let i;
@@ -32,3 +27,28 @@ for(i = 0; i < coll.length; i++) {
    
 }
     
+
+// go to top function
+
+let myButton = document.querySelector(".myBtn");
+
+// when the user scroll down 20px from the top of the document, show the button
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if(document.body.scrollTop > 20 || document.documentElement.scrollTop > 20){
+
+        myButton.style.display = "block";
+
+    } else {
+        myButton.style.display = "none";
+    }
+}
+
+//when the user click on the button, scroll to  the top of the document.
+
+function topFunction() {
+    document.body.scrollTop = 0; //for safari
+    document.documentElement.scrollTop = 0; // chrome and firefox
+}
